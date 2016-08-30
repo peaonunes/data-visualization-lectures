b8d746868f50da43fbf23ed5f5661ee924356b52
peaonunes <rngs@cin.ufpe.br>
2016-08-30 11:44:39 -0300

    Lecture 7

---
+++ lecture7/exercisePipe.js
@@ -0,0 +1,14 @@
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var labelArc = d3.svg.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
